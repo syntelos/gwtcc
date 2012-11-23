07aee3e41cc1c03f33887e6e28b3ae1797fc15fa
John Pritchard <jdp@syntelos.org>
2012-11-23 15:52:34 -0500

    upstream

---
+++ upstream.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Dump svn info to {tgt}
+
+gwt_dir=~/src/google-web-toolkit
+trunk_dir=${gwt_dir}/trunk
+
+tgt=upstream.txt
+
+if [ -d ${trunk_dir} ]
+then
+    if cd ${trunk_dir}
+    then
+        if svn info | egrep -v '(^Path|UUID|Node|Schedule|Author|URL)' | egrep '[A-Z][a-z]*' > ${tgt}
+        then
+            cat -n ${tgt}
+            wc -l ${tgt}
+            exit 0
+        else
+            echo "Error in 'svn info' in '$(pwd)'"
+            exit 1
+        fi
+    else
+        echo "Error in 'cd ${trunk_dir}'"
+        exit 1
+    fi
+else
+    echo "Error, directory not found '${trunk_dir}'"
+    exit 1
+fi
